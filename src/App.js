b598c706f0ebef58b437e48903b3ea376b6368b3
Waleed <wsaleh1991@gmail.com>
2021-11-05 23:46:39 -0500

    ended at creating recipe paramaters

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+// page components
+import NavBar from "./components/navBar/NavBar";
+import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Search from "./pages/search/Search";
+import Recipe from "./pages/recipe/Recipe";
+
+// styles
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h2>Cooking App</h2>
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
